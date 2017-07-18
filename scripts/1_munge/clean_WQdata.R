336032c3321245056cbb70fae6ee9570f49d4817
Oliver <soliver@usgs.gov>
2017-07-18 15:10:49 -0500

    clean up comments and site ID's so that each storm (not sub storm) has a unique ID

--- scripts/1_munge/clean_WQdata.R
+++ scripts/1_munge/clean_WQdata.R
@@ -7,9 +7,6 @@ wq <- read.csv('data_raw/WQdata.csv', header = TRUE)
 # set dates to GMT
 .origin <- as.POSIXct(ifelse(Sys.info()[['sysname']] == "Windows", "1899-12-30", "1904-01-01"))
 tz(.origin) <- 'ETC/GMT+6'
-wq$sample_start <- as.Date(wq$sample_start, origin = .origin, tz = 'America/Chicago')
-wq$sample_start <- as.POSIXct(wq$sample_start, tz = 'American/Chicago')
-wq[,c('sample_start', 'sample_end', 'storm_start', 'storm_end')] <- as.POSIXct(3600*24*(wq[,c('sample_start', 'sample_end', 'storm_start', 'storm_end')]), origin = .origin, tz = "ETC/GMT+6")
 
 date.vars <- c('sample_start', 'sample_end', 'storm_start', 'storm_end')
 for (i in 1:length(date.vars)) {
@@ -18,5 +15,15 @@ for (i in 1:length(date.vars)) {
 }
 
 # clean up comments to keep commenter name but remove '\n'
+wq$comments <- gsub('\n', '',wq$comments)
 
-wq$comments <- gsub('\n', '',dat.keep$comments)
+# clean up site IDs where SW1 was named in slightly different ways
+wq$site <- substr(wq$site, 1, 3)
+
+# create a unique storm id list that does not use subset storms
+wq$unique_storm_id <- gsub("(^.*)(SW1-[[:digit:]]+)([[:alpha:]]*)", "\\2", wq$storm_id, ignore.case = TRUE)
+wq$unique_storm_id <- gsub("(^.*)(SW3-[[:digit:]]+)([[:alpha:]]*)", "\\2", wq$unique_storm_id, ignore.case= TRUE)
+
+write.csv(wq,'data_cached/cleaned_WQdata.csv', row.names = FALSE)
+
+plot(log10(wq$Total.Nitrogen.Load..in.pounds)~log10(wq$peak_discharge))
