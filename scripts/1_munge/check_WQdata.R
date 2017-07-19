29fb225b0f17130c90de816b6892b5834762579c
Oliver <soliver@usgs.gov>
2017-07-19 08:30:50 -0500

    added concentration visualization for data checking

--- scripts/1_munge/check_WQdata.R
+++ scripts/1_munge/check_WQdata.R
@@ -13,3 +13,20 @@ png('figures/log_wq_data_check.png', height = 1000, width = 1000)
 pairs.panels(log.wq.loads, method = 'pearson', hist.col = '#00AFBB', density = FALSE,
              ellipses = FALSE, cex = 1.9, cex.cor = .8, col = "red", cex.lab = 1.4)
 dev.off()
+
+conc <- grep('mg_L', names(wq))
+conc <- wq[,c(conc, p.discharge)]
+drop <- grep('<', conc$Suspended_Sediment_mg_L)
+conc <- conc[-drop, ]
+drop <- grep('<', conc$NO2_NO3_N_mg_L)
+conc <- conc[-drop, ]
+conc <- conc[!is.na(conc$peak_discharge), ]
+conc$Suspended_Sediment_mg_L <- as.numeric(conc$Suspended_Sediment_mg_L)
+conc$NO2_NO3_N_mg_L <- as.numeric(conc$NO2_NO3_N_mg_L)
+log.conc <- log10(conc)
+names(log.conc) <- c("SS mg_L", "Cl mg_L", "NO2+NO3 mg_L", "NH4 mg_L", "TKN mg_L", "DRP mg_L", "TP mg_L", "TN mg_L", "ON mg_L", "Peak Dis.")
+
+png('figures/log_wq_conc_data_check.png', height = 800, width = 800)
+pairs.panels(log.conc, method = 'pearson', hist.col = '#00AFBB', density = FALSE,
+             ellipses = FALSE, cex = 2.4, cex.cor = .8, col = "red", cex.lab = 1.8)
+dev.off()
