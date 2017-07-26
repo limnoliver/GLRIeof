77ef3f20a48169f18a99e0a75e16324f57628edd
Oliver <soliver@usgs.gov>
2017-07-26 09:35:44 -0500

    script to merge wq, rain, and discharge by storm vars

---
+++ scripts/2_process/merge_data.R
@@ -0,0 +1,18 @@
+# script to merge wq, rain, and discharge data
+
+wq <- read.csv('data_cached/prepped_WQbystorm.csv')
+rain <- read.csv('data_cached/rain_variables.csv')
+discharge <- read.csv('data_cached/discharge_variables.csv')
+  
+# subset and rename columns to reduce duplicate cols
+
+names(rain)[1] <- 'unique_storm_id'
+rain <- rain[,c(1, c(4:18))]
+
+names(discharge)[2] <- 'unique_storm_id'
+discharge <- discharge[,c(2, 4:12)]
+
+# merge dat
+
+all.eof <- merge(wq, rain, by = 'unique_storm_id', all.x = TRUE)
+all.eof <- merge(all.eof, discharge, by = 'unique_storm_id', all.x = TRUE)
