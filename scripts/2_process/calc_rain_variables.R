4d1e182074d17c92e89e8485d1d2969de1656ce9
Oliver <soliver@usgs.gov>
2017-09-18 11:06:34 -0500

    read in precipitation through NWIS instead of static files

--- scripts/2_process/calc_rain_variables.R
+++ scripts/2_process/calc_rain_variables.R
@@ -1,6 +1,7 @@
 library(Rainmaker)
 library(lubridate)
 library(dplyr)
+library(dataRetrieval)
 source('scripts/2_process/fxn_RMevents_EOF.R')
 source('scripts/2_process/fxn_runrainmaker.R')
 
@@ -16,14 +17,23 @@ for (i in 1:length(date.vars)) {
 }
 
 # list precipitation data files
-files <- list.files('data_raw')
-precip.files <- grep('precip', files, value = TRUE, ignore.case = TRUE)
+#files <- list.files('data_raw')
+#precip.files <- grep('precip', files, value = TRUE, ignore.case = TRUE)
 
-# set working directory to location of precip data
-precip.wd <- 'H:/Projects/GLRIeof/data_raw'
+# get precipitation data from NWIS using dataretrieval
+siteNumber <- c("441624088045601", "441520088045001")
+parameterCd <- "00045"  # Precipitation
+startDate <- "2010-06-30"
+endDate <- "2017-09-18"
+precip_raw <- readNWISuv(siteNumber, parameterCd, startDate, endDate, tz = "Etc/GMT+6")
+precip_raw <- renameNWISColumns(precip_raw)
+
+# rename columns
+names(precip_raw)[grep('precip_inst$', names(precip_raw), ignore.case = TRUE)] <- 'rain'
+names(precip_raw)[grep('dateTime', names(precip_raw), ignore.case = TRUE)] <- 'pdate'
 
 # summarize precip data for each site
-precip.dat <- run.rainmaker(precip.dir = precip.wd, precip.files = precip.files, siteid = c(5601, 5001),
+precip.dat <- run.rainmaker(precip_raw, siteid = c('441624088045601', '441520088045001'),
                             sitename = c("SW1", "SW3"), ieHr = 2, rainthresh = 0.008, wq.dat = wq.dat,
                             xmin = c(5,10,15,30,60), antecedentDays = c(1,2,7,14))
 
