8f865d1b0c8a982192e9a0c0939c8823df0a998f
Oliver <soliver@usgs.gov>
2017-11-07 16:25:57 -0600

    start calculating field events by iterating through each storm

--- scripts/2_process/calc_field_predictors.R
+++ scripts/2_process/calc_field_predictors.R
@@ -25,23 +25,31 @@ field_events$period_crop[field_events$period_crop == 'after'] <- "after (corn)"
 field_events$period_crop[field_events$storm_start >= as.POSIXct('2016-05-04 00:00:01')] <- "after (alfalfa)"
 
 field_events <- arrange(field_events, storm_start) %>%
-  select(unique_storm_id, sample_start, period, period_crop)
+  select(unique_storm_id, storm_start, period, period_crop)
 
 # calculate days since manure/fertilizer
 # calculate days since planting, set back to zero after
 # cutting
+field_events <- field_events %>%
+  mutate(days_since_planting = NA) %>%
+  mutate(days_since_fertilizer = NA)
+
 for (i in 2:nrow(field_events)) {
   
-  temp.date <- as.Date(format(storms$storm_start[i], "%Y-%m-%d"))
+  temp.date <- as.Date(format(field_events$storm_start[i], "%Y-%m-%d"))
   
   if (field_events$period_crop[i] == "before"|field_events$period_crop[i] == "after (corn)") {
     temp_timeline <- filter(timeline, current_crop == "corn") %>%
       filter(date <= temp.date)
     
     temp_plantings <- temp_timeline[grep('planting', temp_timeline$activity_group), ]
+    temp_harvest <- temp_timeline[grep('harvest', temp_timeline$activity_group), ]
     
     if (nrow(temp_plantings) > 0) {
-      planting_diff <- diff(temp.date - temp_plantings$date)
+      planting_diff <- temp.date - temp_plantings$date
+      field_events$days_since_planting[i] <- as.numeric(min(planting_diff))
+      
+      harvest_diff <- 
     }
     
   } 
