68dddadc2870eb0aeb0b56275f70ddb6cd787d56
Oliver <soliver@usgs.gov>
2017-11-07 12:49:44 -0600

    use storm start and end date to pull and summarize weather data by storm to use as predictors vars

---
+++ scripts/2_process/calc_weather_variables.R
@@ -0,0 +1,30 @@
+library(lubridate)
+library(dplyr)
+
+gb <- read.csv('data_raw/GRB_weather_dat.csv', stringsAsFactors = FALSE, 
+               colClasses = c(date = 'Date'))
+storms <- read.csv('data_cached/prepped_WQbystorm.csv', stringsAsFactors = FALSE,
+                   colClasses = c(storm_start = 'POSIXct', storm_end = 'POSIXct'))
+names(gb)
+# calculate change in snow depth
+snowpack_diff <- diff(gb$snwd)
+gb$snwd_diff[2:nrow(gb)] <- snowpack_diff
+
+# calculate time vars for storms df
+storms$doy <- yday(storms$sample_start)
+
+for (i in 1:nrow(storms)) {
+  dates <- as.Date(unique(c(format(storms$storm_start[i], format = "%Y-%m-%d"), format(storms$storm_end[i], format = "%Y-%m-%d"))))
+  weather <- filter(gb, date %in% dates)
+  storms$tmax[i] <- max(weather$tmax)
+  storms$tmin[i] <- min(weather$tmin)
+  storms$prcp[i] <- sum(weather$prcp)
+  storms$snow[i] <- sum(weather$snow)
+  storms$snwd[i] <- mean(weather$snwd)
+  storms$snwd_diff[i] <- sum(weather$snwd_diff)
+  
+}
+
+weather.dat <- select(storms, unique_storm_id, doy:snwd_diff)
+
+write.csv(weather.dat, 'data_cached/weather_by_storm.csv')
