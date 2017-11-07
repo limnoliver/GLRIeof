5d9b0df1fa696c54c8601f6aed16d1ca79789f4d
Oliver <soliver@usgs.gov>
2017-11-07 15:02:02 -0600

    converted data to long format for ggplot. added two "after" periods that distinguish between corn and alfalfa

--- scripts/3_analyze/calc_before_after_stats.R
+++ scripts/3_analyze/calc_before_after_stats.R
@@ -1,6 +1,8 @@
 # before and after statistics for frozen and unfrozen times
 library(dplyr)
-eof <- read.csv('data_cached/merged_wq_rain_discharge.csv', header = TRUE, 
+library(tidyr)
+library(ggplot2)
+eof <- read.csv('data_cached/merged_dat.csv', header = TRUE, 
                 colClasses = c(storm_start = 'POSIXct', storm_end = 'POSIXct'))
 
 # add columns for frozen, runoff and peak discharge coefs
@@ -12,6 +14,12 @@ sw1 <- eof %>%
   mutate(period = ifelse(storm_start >= as.POSIXct('2015-06-01 00:00:01'), 'after', 'before'))
 
 sw1$period[sw1$storm_start > as.POSIXct('2015-05-10 00:00:01')& sw1$storm_start < as.POSIXct('2015-06-01 00:00:01')] <- 'transition'
+
+# create another period that includes distinguising between
+# after - corn and after - alfalfa
+sw1$period_crop <- sw1$period
+sw1$period_crop[sw1$period_crop == 'after'] <- "after (corn)"
+sw1$period_crop[sw1$storm_start >= as.POSIXct('2016-05-04 00:00:01')] <- "after (alfalfa)"
 
 # calculate statistics for runoff and peak discharge coefs  
 sw1.ba.stats <- sw1 %>%
@@ -42,8 +50,43 @@ sw1.conc.medians <- sw1 %>%
   group_by(frozen, period) %>%
   select(frozen, period, Suspended_Sediment_mg_L:Organic_Nitrogen_computed_mg_L, sum_runoff) %>%
   summarise_all(median, na.rm = TRUE)
+str(sw1.conc.medians)
+
+sw1.conc.long <- sw1 %>%
+  select(frozen, period, period_crop, Suspended_Sediment_mg_L:Organic_Nitrogen_computed_mg_L, sum_runoff) %>%
+  gather(variable, value, -frozen, -period, -period_crop) %>%
+  filter(period != 'transition') %>%
+  mutate(frozen = ifelse(frozen == TRUE, 'frozen', 'thawed'))
+
+sw1.conc.long$period <- ordered(as.factor(sw1.conc.long$period), levels = c('before', 'after'))
+sw1.conc.long$period_crop <- ordered(as.factor(sw1.conc.long$period_crop), levels = c('before', 'after (corn)', 'after (alfalfa)'))
+
+sw1.conc.long$variable <- as.factor(sw1.conc.long$variable)
+sw1.conc.long$variable <- factor(sw1.conc.long$variable, levels(sw1.conc.long$variable)[c(7, 2, 9, 8, 5, 4, 1, 10, 3, 6)])
+levels(sw1.conc.long$variable) <- c('SS (mg/L)', 'Cl (mg/L)', 'TN (mg/L)', 'TKN (mg/L)', 'Org N (mg/L)', 'NO2 + NO3 (mg/L)',
+                                    'NH4 (mg/L)', 'TP (mg/L)', 'DRP (mg/L)', 'Storm Runoff')
 
 
+geom_text(data = n.cens, aes(x = site, y = y, label = label)) 
+  
+n.vals <- sw1.conc.long %>%
+  group_by(variable, frozen, period_crop) %>%
+  summarize(n = length(value), y = ifelse(max(log10(value)) > 1.1, 
+                                          max(log10(value)) + (.15*max(log10(value))), 
+                                          ifelse(max(log10(value)) < 0.1, 
+                                                 0.15, max(log10(value)) + (.8*max(log10(value))))))
 
+p <- ggplot(sw1.conc.long, aes(y = log10(value), x = frozen, group = interaction(period_crop, frozen))) +
+  geom_boxplot(aes(fill = period_crop)) +
+  facet_wrap(~variable, scales = 'free_y') +
+  geom_text(data = n.vals, aes(y = y, label = n), position = position_dodge(width = .8)) +
+  theme_bw() +
+  labs(x = '', y = 'log10 Concentration') +
+  theme(axis.title = element_text(size = 14), axis.text = element_text(size = 12),
+        strip.text = element_text(size = 14))
+
+ggsave('figures/before_after_concentrations.png', p)
+
+head(sw1.conc.medians.long)
 
 
