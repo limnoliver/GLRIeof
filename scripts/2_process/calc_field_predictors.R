38d529c1389b9e31fcf498481bf9f20f2dcf5fbf
Oliver <soliver@usgs.gov>
2017-11-13 16:50:50 -0600

    created a loop to determine days since planting/cutting and days since fertilizer

--- scripts/2_process/calc_field_predictors.R
+++ scripts/2_process/calc_field_predictors.R
@@ -1,3 +1,4 @@
+library(dplyr)
 # calculate days since field action variables
 # e.g., days since planing corn, alfalfa
 # days since last fertilizer/manure application
@@ -25,7 +26,8 @@ field_events$period_crop[field_events$period_crop == 'after'] <- "after (corn)"
 field_events$period_crop[field_events$storm_start >= as.POSIXct('2016-05-04 00:00:01')] <- "after (alfalfa)"
 
 field_events <- arrange(field_events, storm_start) %>%
-  select(unique_storm_id, storm_start, period, period_crop)
+  select(unique_storm_id, site, storm_start, period, period_crop) %>%
+  filter(site == "SW1")
 
 # calculate days since manure/fertilizer
 # calculate days since planting, set back to zero after
@@ -34,9 +36,22 @@ field_events <- field_events %>%
   mutate(days_since_planting = NA) %>%
   mutate(days_since_fertilizer = NA)
 
-for (i in 2:nrow(field_events)) {
+for (i in 1:nrow(field_events)) {
   
   temp.date <- as.Date(format(field_events$storm_start[i], "%Y-%m-%d"))
+  
+  temp_timeline <- filter(timeline, activity_group == "fertilizer application" |activity_group == "planting + fertilizer"|activity_group == "manure application") %>%
+    filter(date <= temp.date)
+  
+  fert_diff <- temp.date - temp_timeline$date
+  field_events$days_since_fertilizer[i] <- as.numeric(min(fert_diff))
+  
+  if (i == 1) {
+    field_events$days_since_planting <- 0 
+    next
+  }
+  
+  
   
   if (field_events$period_crop[i] == "before"|field_events$period_crop[i] == "after (corn)") {
     temp_timeline <- filter(timeline, current_crop == "corn") %>%
@@ -49,11 +64,28 @@ for (i in 2:nrow(field_events)) {
       planting_diff <- temp.date - temp_plantings$date
       field_events$days_since_planting[i] <- as.numeric(min(planting_diff))
       
-      harvest_diff <- 
+      harvest_diff <- temp.date - temp_harvest$date
+      harvest_diff <- ifelse(length(harvest_diff) > 0, as.numeric(min(harvest_diff)), NA)
+      
+      if (!is.na(harvest_diff) & harvest_diff < as.numeric(min(planting_diff))) {
+          field_events$days_since_planting[i] <- 0
+      }
+      
+    } else {
+      field_events$days_since_planting[i] <- 0 
     }
+  } else {
     
-  } 
-  plantings <- timeline[grep('planting')]
-  days_since_planting <- diffs[diffs <= 0 & timeline]
-  
+    temp_timeline <- filter(timeline, date <= temp.date)
+    
+    temp_plantings <- temp_timeline[grep('planting', temp_timeline$activity_group), ]
+    temp_harvest <- temp_timeline[grep('cutting|harvest', temp_timeline$activity_group), ]
+    
+    temp_diff <- c((temp.date - temp_plantings$date), (temp.date - temp_harvest$date))
+    
+    field_events$days_since_planting[i] <- as.numeric(min(temp_diff))
+    
+  }
 }
+
+write.csv(field_events, 'data_cached/field_predictors.csv', row.names = FALSE)
