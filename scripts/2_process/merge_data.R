03e6eb499a34c5ab84dfcb334535bcce9ae398d3
Oliver <soliver@usgs.gov>
2017-11-14 12:51:35 -0600

    added field-level predictors to the merged data

--- scripts/2_process/merge_data.R
+++ scripts/2_process/merge_data.R
@@ -4,6 +4,7 @@ wq <- read.csv('data_cached/prepped_WQbystorm.csv')
 rain <- read.csv('data_cached/rain_variables.csv')
 discharge <- read.csv('data_cached/discharge_variables.csv')
 weather <- read.csv('data_cached/weather_by_storm.csv')
+field <- read.csv('data_cached/field_predictors.csv')
   
 # subset and rename columns to reduce duplicate cols
 
@@ -18,7 +19,7 @@ discharge <- discharge[,c(2, 4:12)]
 all.eof <- merge(wq, rain, by = 'unique_storm_id', all.x = TRUE)
 all.eof <- merge(all.eof, discharge, by = 'unique_storm_id', all.x = TRUE)
 all.eof <- merge(all.eof, weather, by = 'unique_storm_id', all.x = TRUE)
-
+all.eof <- merge(all.eof, field, by = "unique_storm_id", all.x = TRUE)
 # write dat
 
 write.csv(all.eof, 'data_cached/merged_dat.csv', row.names = FALSE)
