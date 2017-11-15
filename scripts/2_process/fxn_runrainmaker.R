202079a8692fa0afd9c7922d8eb401e6d4fb05d5
Oliver <soliver@usgs.gov>
2017-11-15 11:08:41 -0600

    blank space

--- scripts/2_process/fxn_runrainmaker.R
+++ scripts/2_process/fxn_runrainmaker.R
@@ -14,6 +14,7 @@
 # antecedentDays days over which to calculate antecedent rainfall
 library(dplyr)
 library(Rainmaker)
+
 run.rainmaker <- function(precip_raw = precip_raw,
                           siteid = c('441624088045601', '441520088045001'), sitename = c('SW1', 'SW3'), ieHr = 2, rainthresh = 0.008, 
                           wq.dat = wq.dat, xmin = c(5,10,15,30,60), antecedentDays = c(1,3,7,14)) {
