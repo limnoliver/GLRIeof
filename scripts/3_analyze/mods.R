6981425a2da8343e3c4cd77f0283f5464034df91
Oliver <soliver@usgs.gov>
2018-03-08 11:23:26 -0600

    removed data processing steps, put in script 'process_merged_data'

--- scripts/3_analyze/mods.R
+++ scripts/3_analyze/mods.R
@@ -1,5 +1,6 @@
 # this script creates a linear model between water quality and hydrologic variables, 
 # including variables output by Rainmaker, as well as storm characteristics
+source('scripts/3_analyze/holdout_cv_glmnet.R')
 
 # read in libraries
 #library(GSqwsr)
@@ -13,76 +14,31 @@ library(glmnet)
 # source('scripts/3_analyze/fxn_vif.R')
 source('scripts/3_analyze/holdout_cv_glmnet.R')
 
-# read in merged data
-eof <- read.csv('data_cached/merged_dat.csv', header = TRUE, stringsAsFactors = FALSE,
-                colClasses = c(sample_start = 'POSIXct'))
-eof$crop <- eof$period_crop
-eof$crop <- ifelse(eof$crop == "after (alfalfa)", "alfalfa", "corn")
-eof$crop <- as.factor(eof$crop)
 
-# create a water equivalent var that sums rain and 
-# water equivalent of snow, estimated as 1:10
-eof$weq <- ifelse(eof$snwd_diff > 0, eof$rain, eof$rain + (abs(eof$snwd_diff)/10))
+# transform data
 
-# set responses and predictors
-response <- 'Suspended_Sediment_mg_L'
-# start with all predictors - 34 in total
-predictors <- names(eof)[c(39:54, 56:66, 73, 74, 76)]
-
-
-# reduce data to the site of interest, non-frozen periods
-# also add before/transition/after BMP
-
-sw1 <- eof %>%
-  mutate(frozen = as.logical(substr(eof$frozen, 1, 1))) %>%
-  filter(site == 'SW1') %>%
-  filter(frozen == FALSE) %>%
-  mutate(period = ifelse(storm_start >= as.POSIXct('2015-06-01 00:00:01'), 'after', 'before'))
-
-sw1_all <- eof %>% 
-  mutate(frozen = as.logical(substr(eof$frozen, 1, 1))) %>%
-  filter(site == 'SW1') %>%
-  mutate(period = ifelse(storm_start >= as.POSIXct('2015-06-01 00:00:01'), 'after', 'before'))
-
+# do not need to do this if using random forest or other non-linear test
 # remove highly correlated predictors
 predictors.cor <- cor(eof[,predictors[-31]], use = 'complete.obs') # drop var "crop" from correlation since it's a categorical var
 names.cor <- row.names(predictors.cor)
 drop.predictors <- findCorrelation(predictors.cor, cutoff = 0.95, verbose = FALSE, exact = TRUE)
 
-
-sw1$period[sw1$storm_start > as.POSIXct('2015-05-10 00:00:01')& sw1$storm_start < as.POSIXct('2015-06-01 00:00:01')] <- 'transition'
-
-sw1 <- filter(sw1, period != 'transition')
-
 # transform variables that need it - was determined with simple histograms
 # look at histograms
+# also do not need to do this if we are using non-linear technique
 pred.keep <- names.cor[-drop.predictors]
 pred.keep.df <- reshape2::melt(sw1[,pred.keep])
 ggplot(pred.keep.df, aes(x = value)) +
   geom_histogram() +
   facet_wrap(~variable, scales = 'free')
+
 # vars to transform
-# removed ARFdays from transformation even though they were lognormally distributed
-# they have true zeros, so will just keep them unstransformed
-# also do not transform ant_dis_2day_max and erosivity_m1
-# vars.transform <- c('peak_discharge', 'Chloride_mg_L', 'sum_runoff', 'Ievent', 'I5', 'erosivity_m1', 'ARFdays1', 'ARFdays2',
-#                     'ARFdays7', 'ARFdays14', 'ant_dis_2day_max', 'ant_dis_7day_mean', 'ant_dis_7day_max', 'ant_dis_14day_mean',
-#                     'ant_dis_14day_max')
 vars.transform <- c('peak_discharge', 'Chloride_mg_L', 'Ievent', 'I5', 'ant_dis_7day_mean', 'ant_dis_7day_max', 'ant_dis_14day_mean',
                     'ant_dis_14day_max')
 
 # add 0.001 to antecedent rainfall 0s so that I can log transform
 hist(log10(sw1$ARFdays14 + 0.01), breaks = 30)
 length(which(sw1$ant_dis_2day_max == 0))
-
-
-
-# get rid of zeros from database - this should be fixed at some point
-# should not be zeros for erosivity, etc, but there is an issue with getting sub events to count as a single event
-#################Fix this after subevents are fixed, just get rid of next line
-
-#sw1 <- filter(sw1, erosivity_m1 > 0)
-# need to fix storms 60 and 79 from SW1
 
 # transform vars
 sw1[,vars.transform] <- log10(sw1[,vars.transform])
@@ -96,28 +52,11 @@ sw1$erosivity_m1 <- log10(sw1$erosivity_m1 + (0.5*min(sw1$erosivity_m1[sw1$erosi
 sw1$ARFdays7 <- log10(sw1$ARFdays7 + (0.5*min(sw1$ARFdays7[sw1$ARFdays7 > 0])))
 sw1$ARFdays14 <- log10(sw1$ARFdays14 + (0.5*min(sw1$ARFdays14[sw1$ARFdays14 > 0])))
 
-# set responses and set cleaner name to plot for responses
-responses <- names(sw1)[c(2,4:11,13:19)]
-responses_clean <- c('SS (mg/L)', 'NO2 + NO3 (mg/L)','NH4 (mg/L)','TKN (mg/L)',
-                     'DRP (mg/L)','TP (mg/L)','TN (mg/L)','Org N (mg/L)',
-                     'SS (pounds)', 'NO2 + NO3 (pounds)','NH4 (pounds)','TKN (pounds)',
-                     'DRP (pounds)','TP (pounds)','TN (pounds)','Org N (pounds)')
-
+# still do this if using non-linear techniques - response vars
+# are very clearly skewed
 # log transform responses
 sw1[,responses] <- log10(sw1[,responses])
 
-sw1$period_crop <- factor(sw1$period_crop)
-sw1$period_crop <- ordered(sw1$period_crop, levels = c('before', 'after (corn)', 'after (alfalfa)'))
-
-sw1$period <- factor(sw1$period)
-sw1$period <- ordered(sw1$period, levels = c('before', 'after'))
-## create a column that marks suspect splits
-sw1$suspect_split <- grepl('split', sw1$comment)
-
-## get rid of events with suspect splits
-sw1 <- filter(sw1, suspect_split == FALSE)
-
-#plot(sw1$Suspended_Sediment_Load_pounds ~ sw1$peak_discharge, col = as.factor(sw1$suspect_split))
 ######################################################
 ## create a loop that models all responses
 ######################################################
