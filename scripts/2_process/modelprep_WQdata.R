786e5d924106628670c8d4395ba204cf63a22eab
Oliver <soliver@usgs.gov>
2018-03-19 12:57:38 -0500

    now reads in storm parsing file that tells us which storms to lump or separate. Now adds column to show original event IDs (+ substorms) that contributed to that event.

--- scripts/2_process/modelprep_WQdata.R
+++ scripts/2_process/modelprep_WQdata.R
@@ -1,12 +1,26 @@
 # this script preps the WQ data by aggregating by storm
 library(dplyr)
+library(readxl)
 # read in cleaned wq data
 
 wq <- read.csv('data_cached/cleaned_WQdata.csv', stringsAsFactors = FALSE)
+wq.storm.sw1 <- read_excel('data_raw/sw1_event_parsing.xlsx')
+
+# find which storms to keep from storm parsing file
+keep.storms <- filter(wq.storm.sw1, exclude == 0) %>%
+  filter(estimated == 0) %>%
+  filter(discrete == 0) %>%
+  select(unique_storm_id, unique_storm_number)
+
+wq <- filter(wq, storm_id %in% keep.storms$unique_storm_id) %>%
+  filter(!is.na(runoff_volume)) %>%
+  filter(!(lab_id == '376-13-10' & storm_id == 'ESW1-15')) %>% #some duplicated id's that needed to be removed
+  rename(unique_storm_id = storm_id) %>%
+  left_join(keep.storms, by = 'unique_storm_id')
 
 # Drop estimated samples
 # Drop discrete samples
-wq <- filter(wq, discrete == FALSE, estimated == FALSE)
+# wq <- filter(wq, discrete == FALSE, estimated == FALSE)
 
 # drop unused levels for factors
 
@@ -32,12 +46,13 @@ wq$Chloride_mg_L <- as.numeric(wq$Chloride_mg_L)
 
 
 # add a column that will be used for weighting concentrations by total runoff volume
-storm.vols <- wq[,c('unique_storm_id', 'runoff_volume')]
+storm.vols <- wq[,c('unique_storm_number', 'runoff_volume', 'unique_storm_id')]
 storm.vols <- storm.vols %>%
-  group_by(unique_storm_id) %>%
-  summarise(sum_runoff = sum(runoff_volume))
+  group_by(unique_storm_number) %>%
+  summarise(sum_runoff = sum(runoff_volume), 
+            sub_storms = paste(unique_storm_id, collapse = ","))
 
-wq <- merge(wq, storm.vols, by = 'unique_storm_id', all.x = TRUE)
+wq <- merge(wq, storm.vols, by = 'unique_storm_number', all.x = TRUE)
 wq <- mutate(wq, vol_weight = runoff_volume/sum_runoff)
 # get rid of any samples that do not have a volume weight, 
 # which will give us NA values later on
@@ -58,18 +73,18 @@ flagvars <- grep('flag|frozen|comment', names(wq), ignore.case = TRUE, value = T
 
 
 loadbystorm <- wq %>% 
-  group_by(unique_storm_id) %>%
+  group_by(unique_storm_number) %>%
   summarise_at(vars(loadvars), sum, na.rm = TRUE) 
 
-concbystorm <- wq[,c(concvars, 'unique_storm_id', 'vol_weight')]
+concbystorm <- wq[,c(concvars, 'unique_storm_number', 'vol_weight')]
 concbystorm[, concvars] <- concbystorm[,concvars]*concbystorm[,'vol_weight']
 
 concbystorm <- concbystorm %>%
-  group_by(unique_storm_id) %>%
+  group_by(unique_storm_number) %>%
   summarise_at(vars(concvars), sum, na.rm = TRUE)
 
 stormdesc <- wq %>%
-  group_by(unique_storm_id) %>%
+  group_by(unique_storm_number) %>%
   summarise(
     sample_start = min(sample_start),
     sample_end = max(sample_end),
@@ -79,13 +94,13 @@ stormdesc <- wq %>%
   )
 
 flagsbystorm <- wq %>%
-  group_by(unique_storm_id) %>%
+  group_by(unique_storm_number) %>%
   summarise_at(vars(flagvars), toString) 
 
 wq.bystorm <- merge(concbystorm, loadbystorm)
 wq.bystorm <- merge(wq.bystorm, storm.vols)
 wq.bystorm <- merge(wq.bystorm, flagsbystorm)
-wq.bystorm <- merge(wq.bystorm, unique(wq[,c('site', 'water_year', 'unique_storm_id')]), all.x = TRUE)
+wq.bystorm <- merge(wq.bystorm, unique(wq[,c('site', 'water_year', 'unique_storm_number', 'sub_storms')]), all.x = TRUE)
 wq.bystorm <- merge(wq.bystorm, stormdesc)
 
 write.csv(wq.bystorm, 'data_cached/prepped_WQbystorm.csv', row.names = FALSE)
