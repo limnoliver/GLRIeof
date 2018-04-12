98a6e5ad107d9519f1270c3a61e1e9fee5113b60
Oliver <soliver@usgs.gov>
2018-04-12 13:48:26 -0500

    added environments for each major source so does not add to global environment.

--- scripts_for_collaborators/scripts/processing_run_file.R
+++ scripts_for_collaborators/scripts/processing_run_file.R
@@ -8,11 +8,19 @@ library(USGSHydroTools)
 library(lubridate)
 library(rnoaa)
 
-# source master file
-source('0_master_file.R')
+# source the master file with all site-specific vars
+source('scripts/0_master_file.R', echo = F)
+
+# source the water quality file which is the basis for all other processing.
+wq_env <- new.env()
+source('scripts/1_calc_storm_wq.R', echo = F, local = wq_env)
 
 # source functions that are needed
 source('scripts/fxns_data_processing.R')
 
 # source the data merge step, which sources all of the data processing steps
-source('scripts/')
+rain_env <- new.env()
+source('scripts/2_calc_rain_variables.R', echo = F, local = rain_env)
+
+weather_env <- new.env()
+source('scripts/2_calc_weather_variables.R', echo = F, local = weather_env)
