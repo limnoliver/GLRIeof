f4f4eb41c059583d99d5b4ce415abed55b16d780
Oliver <soliver@usgs.gov>
2018-04-12 13:48:08 -0500

    removed unnecessary variables

--- scripts_for_collaborators/scripts/fxns_data_processing.R
+++ scripts_for_collaborators/scripts/fxns_data_processing.R
@@ -2,7 +2,7 @@
 # data processing steps
 
 # function to run through Rainmaker and extract events, event characteristics
-run.rainmaker <- function(precip_raw, siteid, site, wq.dat, ieHr = 2, rainthresh = 0.008, 
+run.rainmaker <- function(precip_raw, wq.dat, ieHr = 2, rainthresh = 0.008, 
                           xmin = c(5,10,15,30,60), antecedentDays = c(1,3,7,14)) {
   
   # calculate events
