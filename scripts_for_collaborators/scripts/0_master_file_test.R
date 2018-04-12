4c9c6fac732829f8cec3a002ef214c65ebf646ec
Oliver <soliver@usgs.gov>
2018-04-12 13:44:41 -0500

    Fixed date format

--- scripts_for_collaborators/scripts/0_master_file_test.R
+++ scripts_for_collaborators/scripts/0_master_file_test.R
@@ -21,7 +21,7 @@ site_no_paired <- NA # USGS paired (control) site number, 'NA' if no paired site
 
 site_tz <- 'Etc/GMT+6' # all data should be in (or called into R) the same time zone. 
                        # Indicate here which timezone that is - e.g. central "America/Chicago" or central without DST "Etc/GMT+6"
-datetime_format <- "%m/%d/%Y HH:MM" # format of all datetime columns.
+datetime_format <- "%m/%d/%Y %H:%M" # format of all datetime columns.
 date_format <- '%m/%d/%Y' # format of date variables in provided data files. For example, the default in Excel is
 # mm/dd/yyyy, and the coded format of that date in R is "%m/%d/%Y". 
 # dates from Excel are read in the format "" -- but R wants them in the format "YYYY"
@@ -30,7 +30,7 @@ date_format <- '%m/%d/%Y' # format of date variables in provided data files. For
 
 start_date <- as.POSIXct('2014-03-11 00:00:01') # YYYY-MM-DD HH:MM:SS date/time of study start
                                                 
-bmp_date <- as.POSIXct('') # YYYY-MM-DD HH:MM:SS date/time of BMP implementation. All events before
+bmp_date <- as.POSIXct('2017-05-26 00:00:01') # YYYY-MM-DD HH:MM:SS date/time of BMP implementation. All events before
                            # this date will be considered 'before', all events after this 
                            # date will be considered 'after'
 end_date <- as.POSIXct('2017-08-04 00:00:01') # YYYY-MM-DD HH:MM:SS date/time of study end
@@ -55,21 +55,21 @@ clean_names <- c('SS (mg/L)', 'TP (mg/L)', 'SS load (pounds)', 'TP load (pounds)
                      # model. This will be used for plotting purposes to create nice looking
                      # axes. For example, if a column was named suspended_sediments_load_pound, a 
                      # better axis name would be "SS load (pounds)"
+
 ###########################################################################################
 # If you're using a before-after study design, the variables below should be filled out.
 # If you have a paired site, these data aren't necessary, but can be used to run 
 # site diagnostics (yearly precipitation, etc)
 ###########################################################################################
 # rain data
-rain_site <- '' # site number for rain gage if pulling from NWIS, 'NA' if providing data file
-rain_file <- '' # filename of rain file, 'NA' if pulling from NWIS. Should be a csv and include ".csv" at end of file name.
+rain_site <- "441520088045001" # site number for rain gage if pulling from NWIS, 'NA' if providing data file
+rain_file <- NA # filename of rain file, 'NA' if pulling from NWIS. Should be a csv and include ".csv" at end of file name.
 
 rain_column <- '' # if using own data, the name of the rain value column
 date_column <- '' # if using own data, the name of the date/time column, should be in standard POSIXct (YYY-MM-DD HH:MM:SS)
-rain_tz <- '' # time zone of date_column. See ?timezones for more information. Central time is
 
 # field activity data
-activity_file <- '' # filename of field activity log and should include .csv extension
+activity_file <- 'test_field_activity' # filename of field activity log and should include .csv extension
 nut_additions_keywords <- c('manure', 'fertilizer') # words used to describe any manure and/or fertilizer additions 
                                                     # to the field. These should be keywords found in the "activity_group" column
 cultivation_keywords <- c('cultivation') # words to describe tillage and other field cultivation activities
@@ -78,26 +78,27 @@ planting_keywords <- c('planting') # words to describing planting activities, fo
 harvest_keywords <- c('cutting', 'harvest') # words describing cutting/harvest activities, found in "activity_group" column.
 
 # discharge data
-discharge_file <- '' # filename of discharge data of a nearby stream gage to calculate antecedent discharge, NA if pulling from NWIS using site_no
-discharge_site_no <- '' # if not providing a discharge file, the USGS stream gage site to pull discharge data from
+discharge_file <- NA # filename of discharge data of a nearby stream gage to calculate antecedent discharge, NA if pulling from NWIS using site_no
+discharge_site_no <- '04085108' # if not providing a discharge file, the USGS stream gage site to pull discharge data from
 antecedent_days <- c(1,2,3,7,14) # the days over which to calculate antecedent discharge. These are the values used for the WI sites.
 
 dis_date_column <- 'Date' # name of column with dates if providing own file - defaults to NWIS standards
 discharge_column <- 'Flow' # name of column with discharge data if providing own file - defaults to NWIS standards
 
 # weather data
-weather_file <- '' # filename (ending in .csv) of daily weather data from a nearby met station to calculate storm
+weather_file <- NA # filename (ending in .csv) of daily weather data from a nearby met station to calculate storm
                    # weather characteristics, NA if pulling data directly from NOAA (preferred). 
                    # At a minimum, this file should include 'date' and 
                    # daily min (tmin) and max (tmax) temperature in degrees celsius. 
-                   # Can also include snow depth  in milimeters (snwd).
+                   # Can also include snow depth  in millimeters (snwd).
 
 
-noaa_site <- '' # nearest NOAA met station site number. Can use both weather_file and noaa_site if 
-                # weather_file does not contain snow depth. 
+noaa_site <- 'USW00014898' # nearest NOAA met station site number. Can use both weather_file and noaa_site if 
+                           # weather_file does not contain snow depth. 
 
 # other
-predictors_drop <- '' # one or more predictor variables that should be dropped from the analysis based
+predictors_drop <- c('days_since_cultivation', 'days_since_planting')
+                      # one or more predictor variables that should be dropped from the analysis based
                       # on site-specific analysis goals. For example, if you expect that the implemented
                       # BMP will affect field activity (e.g., if the farmer moves to no-till, 'days_since_cultivation'
                       # will be different before vs after) it should not be included in the residual model.
