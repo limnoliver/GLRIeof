e3abebecb8dad0520a0df7a53b2cc4cc8229823b
Oliver <soliver@usgs.gov>
2018-04-27 08:23:14 -0500

    added script for diagnostic plots for non-paired sites

---
+++ scripts_for_collaborators/scripts/data_processing/5_diagnostics_plots.R
@@ -0,0 +1,152 @@
+# diagnostic plots for paired design
+
+tempfile <- file.path('data_cached', paste0(site, "_mod_dat.csv"))
+wq <- read.csv(tempfile, stringsAsFactors = F)
+
+
+# for each constituent, plot time vs conc or load, size of dot = discharge
+# get load and conc vars
+if (length(loads) == 1 & !is.na(loads)){
+  
+  loadvars <- grep(loads, names(wq), ignore.case = TRUE, value = TRUE)
+
+} else if (is.na(loads)) {
+  loadvars <- NA
+} else {
+  loadvars <- loads
+}
+
+if (length(concentrations) == 1 & !is.na(concentrations)){
+  
+  concvars <- grep(concentrations, names(wq), ignore.case = TRUE, value = TRUE)
+  
+} else if (is.na(concentrations)) {
+  concvars <- NA
+} else {
+  concvars <- concentrations
+}
+
+wq[,'storm_start'] <- as.POSIXct(wq[,'storm_start'])
+wq$Date <- wq[,'storm_start']
+wq <- dataRetrieval::addWaterYear(wq)
+
+plot_all_vars <- c(concvars, loadvars)
+plot_all_vars <- plot_all_vars[!is.na(plot_all_vars)]
+
+
+
+for (i in 1:length(plot_all_vars)) {
+  
+  temp_var <-  plot_all_vars[i]
+
+  p <- ggplot(data = wq, aes_string(x = 'storm_start', y = temp_var)) +
+    geom_point(aes(color = period, shape = site, size = runoff_volume)) +
+    scale_size_continuous(trans = 'log10') +
+    coord_trans(y = 'log10') +
+    #scale_y_continuous(breaks = c(0.1, 1, 10, 100, 300, 600)) +
+    scale_shape_manual(values = c(21, 16)) +
+    theme_bw() +
+    theme(panel.grid.minor.y = element_blank()) +
+    labs(x = 'Storm Date', y = clean_names[i])
+  
+  if (!is.na(discharge_col)){ 
+    p <- p + 
+  } else {
+    p <- p + geom_point(aes(color = period, shape = site))
+  }
+  
+  short_col_name <- paste0(control_site, '_', temp_trt, '_throughtime.png')
+  tempname <- file.path('figures', 'diagnostic', short_col_name)
+  ggsave(tempname, p, height = 4, width = 8)
+}
+
+# calculate summary statistics by water year and export to tables/figures for review
+
+sum_stats <- wq %>%
+  group_by(waterYear) %>%
+  summarize(n_before = length(which(period == 'before')),
+            n_after = length(which(period == 'after')))
+
+if (!is.na(con_loadvars[1])){
+  temp <- wq %>%
+    group_by(waterYear) %>%
+    summarize_at(.vars = c(con_loadvars, trt_loadvars), sum, na.rm = T)
+  
+  names(temp)[which(names(temp) %in% c(con_loadvars, trt_loadvars))] <- 
+    paste0('sum_', c(con_loadvars, trt_loadvars))
+
+  sum_stats <- left_join(sum_stats, temp)
+}
+
+if(!is.na(con_concvars[1])) {
+  
+  sum_stats <- wq %>%
+    group_by(waterYear) %>%
+    summarize_at(.vars = c(con_concvars, trt_concvars), mean, na.rm = T) %>%
+    left_join(sum_stats)
+}
+
+if (!is.na(discharge_col)) {
+  disvars <- grep(discharge_col, names(wq), ignore.case = T, value = T)
+  sum_stats <- wq %>%
+    group_by(waterYear) %>%
+    summarize_at(.vars = disvars, sum, na.rm = T) %>%
+    left_join(sum_stats)
+}
+
+temp_table_name <- paste0(site, "_", site_paired, '_response_summary.csv')
+write.csv(sum_stats, file.path('figures', 'diagnostic', temp_table_name), row.names = F)
+
+# create frozen/non frozen loads  plot
+# ignore first and last year if the difference between start and end months
+# is greater than 3
+
+dif <- abs(month((min(wq$storm_start))) - month((max(wq$storm_start))))
+
+library(tidyr)
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
+#group by frozen/non frozen and sum loads over both seasons
+if (dif > 3) {
+  wq.temp <- filter(wq, waterYear != min(waterYear)) %>%
+    filter(waterYear != max(waterYear))
+} else {
+  wq.temp <- wq
+}
+prop <- wq.temp %>%
+  group_by(frozen) %>%
+  summarise_at(vars(loadvars), sum)
+
+# change names to be shortened/clean versions
+names(prop)[2:ncol(prop)] <- clean_names[(length(concvars) + 1):(length(clean_names))]
+prop.long <- prop %>%
+  gather(variable, value, -frozen)
+
+prop.long$frozen <- as.factor(prop.long$frozen)
+levels(prop.long$frozen) <- c('not frozen', 'frozen') 
+prop.long$frozen <- as.character(prop.long$frozen)
+
+prop.long.tot <- prop.long %>%
+  group_by(variable) %>%
+  summarise_at('value', sum)
+
+names(prop.long.tot)[2]<- 'total'
+
+prop.bar.plot <- left_join(prop.long, prop.long.tot) %>%
+  mutate(percentage = value/total)
+
+var.order <- filter(prop.bar.plot, frozen == 'not frozen') %>%
+  arrange(percentage)
+
+prop.bar.plot$variable <- factor(prop.bar.plot$variable, levels = var.order$variable)
+
+p <- ggplot(data = prop.bar.plot, aes(x = variable, y = percentage, fill = frozen)) +
+  geom_bar(stat = 'identity') +
+  #geom_hline(aes(yintercept = mean(var.order$percentage), linetype = "Mean proportion of \nnon-frozen loads"), size = 2, color = 'darkgray') +
+  scale_fill_manual(values = c(gg_color_hue(2)[2], gg_color_hue(2)[1])) +
+  theme_bw() +
+  labs(x = '', y = 'Proportion of Load', linetype = "")
+p
