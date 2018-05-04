123562f91c0fb20c75a9ff6dc4912d8b0605d9b3
Oliver <soliver@usgs.gov>
2018-05-04 15:12:02 -0500

    script to check inputs the user provides in 0_master_file

---
+++ scripts_for_collaborators/scripts/data_processing/0_check_inputs.R
@@ -0,0 +1,26 @@
+# tests to run on master file to make sure user has input
+# variables correctly
+source('scripts/functions/fxns_tests.R')
+
+stopifnot(study_type %in% c('before_after', 'paired'))
+
+if (study_type == "before_after") {
+  # check if user has included ".csv" in file extensions
+  # water quality dat
+  test.csv(wq_file)
+  
+  # rain dat
+  if (is.na(rain_site)){
+    test.csv(rain_file)
+  }
+  # field activity file
+  test.csv(activity_file)
+  
+  # discharge dat
+  if (is.na(discharge_site_no)) {
+    test.csv(discharge_file)
+  }
+  
+  
+}
+
