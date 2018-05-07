5014328932c133a7e84b840b54cb6e133d56cd8f
Oliver <soliver@usgs.gov>
2018-05-07 14:42:04 -0500

    removed "end" messages for each step. Worked these into the scripts themselves so that a "stop" could be added so scripts would fail appropriately and give useful messages.

--- scripts_for_collaborators/scripts/run_files/processing_run_file.R
+++ scripts_for_collaborators/scripts/run_files/processing_run_file.R
@@ -41,24 +41,20 @@ source('scripts/data_processing/3_merge_data.R', echo = F, local = merge_env)
 message('Prepping merged data for analysis.')
 mod_dat_env <- new.env()
 source('scripts/data_processing/4_process_merged_data.R', echo = F, local = mod_dat_env)
-message(paste0('Data processing complete. Please check file data_cached/', site, '_mod_dat.csv to verify all import, processing, and merging went as planned prior to entering the analysis phase.'))
 
 # source the diagnostic plots
 message('Creating diagnostic plots for the before/after study')
 diag_env <- new.env()
 source('scripts/data_processing/5_diagnostics_plots.R', echo = F, local = diag_env)
-message('Diagnostic plots complete. Please see the figures in figures/diagnostic as one way to verify all import, processing, and merging went as planned prior to entering the analysis phase. If you would like to add figures to diagnostics, you can modify the script in scripts/data_processing/5_diagnostic_plots.R')
 
 } else {
   # import water quality
   message('Importing and processing the storm water quality data for the paired study.')
   wq_env <- new.env()
   source('scripts/data_processing/1_calc_storm_wq_paired.R', echo = F, local = wq_env)
-  message(paste0('Data processing complete. Please check file data_cached/', site, '_', site_paired, '_prepped_WQbystorm.csv to verify all import, processing, and merging went as planned prior to entering the analysis phase.'))
-  
+
   # create diagnostic plots
   message('Creating diagnostic plots/data for paired study.')
   diag_env <- new.env()
   source('scripts/data_processing/5_paired_diagnostics_plots.R', echo = F, local = diag_env)
-  message('Diagnostic data/plots complete. Please see files in figures/diagnostic to verify all import, processing, and merging went as planned prior to entering the analysis phase.')
 }
