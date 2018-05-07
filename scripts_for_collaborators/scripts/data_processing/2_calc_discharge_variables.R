6e15ba0bdf117a127ad55d91428c69866ac99a02
Oliver <soliver@usgs.gov>
2018-05-07 09:52:30 -0500

    added tests for both the NWIS pull and final discharge processing steps to verify that empty data frame is not being written.

--- scripts_for_collaborators/scripts/data_processing/2_calc_discharge_variables.R
+++ scripts_for_collaborators/scripts/data_processing/2_calc_discharge_variables.R
@@ -11,6 +11,11 @@ storms <- wq.dat[,c('unique_storm_number', 'storm_start')]
 if (is.na(discharge_file)) {
   discharge.dat <- readNWISdv(siteNumbers = discharge_site_no, parameterCd = '00060')
   discharge.dat <- renameNWISColumns(discharge.dat)
+  
+  if (nrow(discharge.dat) == 0) {
+    stop('Discharge data not successfully pulled from NWIS. Please check inputs to verify site number. To debug, see code in "data_processing/2_calc_discharge_variables.R"')
+  }
+  
 } else {
   temp_filepath <- file.path('data_raw', discharge_file)
   discharge.dat <- read.csv(temp_filepath)
@@ -34,6 +39,14 @@ discharge_vars <- select(discharge_vars, -ant_dis_1day_mean, -storm_start)
 # write antecedent discharge data
 temp_filename <- file.path('data_cached', paste0(site, '_discharge_variables.csv'))
 write.csv(discharge_vars, temp_filename, row.names = FALSE)
+
+test <- discharge_vars[!is.na(discharge_vars$ant_dis_1day_max), ]
+
+if (nrow(test) > 0) {
+  message(paste("The discharge data have been processed. Please check", temp_filename, "to ensure correct processing."))
+} else {
+  stop('Something went wrong with discharge data processing. To debug, please see code in "data_processing/2_calc_discharge_variables.R"')
+}
 
  
 
