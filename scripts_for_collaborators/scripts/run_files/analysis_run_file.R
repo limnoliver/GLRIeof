80ee4acbf52dc0447bc83536975f5a13fe8e1020
Oliver <soliver@usgs.gov>
2018-05-10 15:49:39 -0500

    added running of data prep, mdc and analysis scripts

--- scripts_for_collaborators/scripts/run_files/analysis_run_file.R
+++ scripts_for_collaborators/scripts/run_files/analysis_run_file.R
@@ -1,13 +1,26 @@
 # Run this file to perform all data analysis
 
 if (study_type == 'before_after') {
-# source the water quality file which is the basis for all other processing.
-message('Calculating minimum detectable change using the pre-BMP data.')
-mdc_env <- new.env()
-source('scripts/data_analysis/1_mdc_before_after.R', echo = F, local = mdc_env)
-
-
-
+  mod_env <- new.env()
+  
+  # source the mod.dat prep step that will feed into both MDC calculations and modeling
+  source('scripts/data_analysis/0_before_after_datmod_prep.R', echo = F, local = mod_env)
+  
+  # source script to calculate MDC.
+  message('Calculating minimum detectable change using the pre-BMP data. This calculation assumes the same number of observations in the post-BMP period.')
+  source('scripts/data_analysis/1_mdc_before_after.R', echo = F, local = mod_env)
+  
+  # source script to do the residual test, and if significant, the calculations for percent change
+  # script also generates model figures and sends to figures
+  message('Testing whether BMP implementation reduced loads/concentrations.')
+  source('scripts/data_analysis/2_before_after_mods.R', echo = F, local = mod_env)
+  
+  
 } else {
-
+  message('Calculating minimum detectable change using the pre-BMP data. This calculation assumes the same number of observations in the post-BMP period.')
+  mdc_env <- new.env()
+  source('scripts/data_analysis/1_mdc_paired.R', echo = F, local = mdc_env)
+  
+  
+  
 }
