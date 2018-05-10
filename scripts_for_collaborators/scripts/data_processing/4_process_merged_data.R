567f4141b5a6aed755638649ef99f558f85dfc98
Oliver <soliver@usgs.gov>
2018-05-10 14:04:55 -0500

    moved to save function versus saveRDS as saveRDS wasn't allowing read in

--- scripts_for_collaborators/scripts/data_processing/4_process_merged_data.R
+++ scripts_for_collaborators/scripts/data_processing/4_process_merged_data.R
@@ -46,7 +46,7 @@ write.csv(site_dat, temp_filename, row.names = F)
 ############
 # save predictors and responses for later use
 
-saveRDS(c(predictors, responses), 'data_cached/modvars.Rdata')
+save(predictors, responses, file = 'data_cached/modvars.Rdata')
 
 if (nrow(site_dat) > 1) {
   message(paste("The data have now been merged and processed. Please see", temp_filename, "to ensure processing."))
