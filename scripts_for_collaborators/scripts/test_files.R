4f77a20d1ce91b811a259cfdd54dcc5c40632fa8
Oliver <soliver@usgs.gov>
2018-05-11 11:39:20 -0500

    added some tests to consider if the user is having issues or not getting what was expected.

---
+++ scripts_for_collaborators/scripts/test_files.R
@@ -0,0 +1,11 @@
+# troubleshooting ideas
+
+# are your variables in the same order as your clean_names?
+# to test, pull concentration, load, other_responses vars and see if they match your
+# clean vars. E.g., 
+
+# did the predictor variables pull properly - and return what you expected?
+# to test, get predictors, print them out and verify they are what you want them to be
+
+# dates - are the pulled in dates what you expected, and do they match across variables? E.g., did 
+# the time zones get properly read in and aligned?
