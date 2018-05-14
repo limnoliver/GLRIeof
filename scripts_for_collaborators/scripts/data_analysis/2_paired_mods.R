2ffaf3814ddb94949a908a217e761e502ce74e59
Oliver <soliver@usgs.gov>
2018-05-14 09:07:13 -0500

    added a data frame to write model results. saved figure with rsquared value and pvalue for testing if periods were different

--- scripts_for_collaborators/scripts/data_analysis/2_paired_mods.R
+++ scripts_for_collaborators/scripts/data_analysis/2_paired_mods.R
@@ -1,6 +1,9 @@
 # loop through each response var
 # uses vars set up in 1_mds_paired.R
 dat <- wq
+out <- data.frame(matrix(nrow = length(trt_vars), ncol = 7))
+names(out) <- c('response', 'rsq', 'pval_period_int', 'intercept', 'slope', 
+                'int_period_before', 'slope_period_before')
 for (i in 1:length(trt_vars)) {
   
   temp_trt <-  trt_vars[i]
@@ -14,11 +17,29 @@ for (i in 1:length(trt_vars)) {
   # create linear model
   mod_temp = lm(trt ~ con*period, data=temp_dat)
   mod_anova <- anova(mod_temp)
+  pval_period <- round(mod_anova$`Pr(>F)`[3], 2)
+  rsq <- round(summary(mod_temp)$r.squared, 2)
   
+  out[i, 1] <- clean_names[i]
+  out[i, 2:7] <- c(rsq, pval_period, as.numeric(round(summary(mod_temp)$coefficients[,1], 3)))
+  
+  p.label <- ifelse(pval_period <= 0.05, paste0("Sig. difference after BMP implementation (p = ", pval_period, ")"),
+                    paste0("No sig. difference after BMP \nimplementation (p = ", pval_period, ")"))
+                                        
+  rsq_label <- paste("R^2 == ", rsq)
   # make plot of interactions with data points
-  interact_plot(mod_temp, pred = con, modx = period, plot.points = T) +
-    scale_color_manual(values = c('red','black')) +
-    scale_fill_manual(values = c('red', 'black')) +
+  p <- interact_plot(mod_temp, pred = con, modx = period, plot.points = T) +
+    scale_color_manual(values = c('before' = 'black', 'after' = 'red')) +
+    scale_fill_manual(values = c('before' =  'black', 'after' = 'red')) +
     scale_linetype_manual(values = c(1,1)) +
-    labs(x = paste0('Control site - ', clean_names[i]), y = paste0('Test site - ', clean_names[i]))
+    labs(x = paste0('Control site - log ', clean_names[i]), y = paste0('Test site - log ', clean_names[i])) +
+    annotate('text', x = -Inf, y = Inf, label = rsq_label, vjust = 1.1, hjust = -0.1, parse = T) +
+    annotate('text', x = -Inf, y = Inf, label = p.label, vjust = 2, hjust = -0.04)
+  
+  # save figure 
+  temp_filename <- file.path('figures', paste0(site,'_', site_paired, '_regression_lines.png'))
+  ggsave(temp_filename, p, height = 4, width = 6)
+  
 }
+temp_filename <- file.path('data_cached', paste0(site,'_', site_paired, '_mod_outputs.csv'))
+write.csv(out, temp_filename, row.names = F)
