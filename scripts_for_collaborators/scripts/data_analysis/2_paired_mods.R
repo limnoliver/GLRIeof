1e5184f93e39419ac159a1f63ba3b9b9b12a4c0a
Oliver <soliver@usgs.gov>
2018-05-14 13:04:06 -0500

    added percent change to output dataframe

--- scripts_for_collaborators/scripts/data_analysis/2_paired_mods.R
+++ scripts_for_collaborators/scripts/data_analysis/2_paired_mods.R
@@ -1,9 +1,9 @@
 # loop through each response var
 # uses vars set up in 1_mds_paired.R
 dat <- wq
-out <- data.frame(matrix(nrow = length(trt_vars), ncol = 7))
+out <- data.frame(matrix(nrow = length(trt_vars), ncol = 9))
 names(out) <- c('response', 'rsq', 'pval_period_int', 'intercept', 'slope', 
-                'int_period_before', 'slope_period_before')
+                'int_period_before', 'slope_period_before', 'mean_event_pct_change', 'total_pct_change')
 for (i in 1:length(trt_vars)) {
   
   temp_trt <-  trt_vars[i]
@@ -53,6 +53,42 @@ for (i in 1:length(trt_vars)) {
     scale_color_manual(values = c('black', 'red'), labels = c('before', 'after')) +
     labs(x = '', y = 'Treatment - Control \n(standardized to calibration period)')
   
+  if (out$pval_period_int[i] > 0.05) {
+    
+    out$mean_event_pct_change[i] <- NA
+    out$total_pct_change[i] <- NA
+    
+  } else {
+  
+    # reduce to just "after" data
+    temp_after <- filter(temp_dat, period == "after")
+    
+    # create two datasets - where period is either before or after for prediction using
+    # the interaction model
+    pred_before <- temp_after
+    pred_before$period <- 'before'
+    
+    pred_after <- temp_after
+    pred_after$period <- 'after'
+    
+    # predict the post events based on both fitted lines
+    predict_baseline <- predict(mod_temp, pred_before)
+    predict_treatment <- predict(mod_temp, pred_after)
+    
+    temp_after$pred_baseline <- as.numeric(predict_baseline)
+    temp_after$pred_treatment <- as.numeric(predict_treatment)
+    
+    # calculate the difference and percent difference
+    temp_after$pred_diff <- temp_after$pred_treatment - temp_after$pred_baseline
+    temp_after$pred_diff_perc <- (temp_after$pred_diff/temp_after$pred_baseline)*100
+    
+    # calculate percent reduction in two ways -- 1) mean of the event-level
+    # percent differences 2) percent difference of sum of loads
+    
+    out$mean_event_pct_change[i] <- mean(temp_after$pred_diff_perc)
+    out$total_pct_change[i] <- ((sum(temp_after$pred_treatment) - sum(temp_after$pred_baseline))/sum(temp_after$pred_baseline))*100
+    
+  
 }
 
 ##############################
@@ -77,41 +113,21 @@ for (i in 1:length(trt_vars)) {
   pred_before <- temp_after
   pred_before$period <- 'before'
   
-  pred_after <- temp_dat
-  pred_before$period <- 'after'
+  pred_after <- temp_after
+  pred_after$period <- 'after'
   
   predict_baseline <- predict(mod_temp, pred_before)
   predict_treatment <- predict(mod_temp, pred_after)
   
-  plot(temp_after$trt ~ temp_after$con, pch = 16)
-  points(predict_baseline ~ temp_after$con, pch = 16, col = 'red')
-  # create linear model
-  mod_before <- lm(trt ~ con, data=temp_before)
-  mod_after <-  lm(trt ~ con, data=temp_after)
+  temp_after$pred_baseline <- as.numeric(predict_baseline)
+  temp_after$pred_treatment <- as.numeric(predict_treatment)
   
-  mod_anova <- anova(mod_temp)
-  pval_period <- round(mod_anova$`Pr(>F)`[3], 2)
-  rsq <- round(summary(mod_temp)$r.squared, 2)
+  temp_after$pred_diff <- temp_after$pred_treatment - temp_after$pred_baseline
+  temp_after$pred_diff_perc <- (temp_after$pred_diff/temp_after$pred_baseline)*100
   
-  out[i, 1] <- clean_names[i]
-  out[i, 2:7] <- c(rsq, pval_period, as.numeric(round(summary(mod_temp)$coefficients[,1], 3)))
-  
-  p.label <- ifelse(pval_period <= 0.05, paste0("Sig. difference after BMP implementation (p = ", pval_period, ")"),
-                    paste0("No sig. difference after BMP \nimplementation (p = ", pval_period, ")"))
-  
-  rsq_label <- paste("R^2 == ", rsq)
-  # make plot of interactions with data points
-  p <- interact_plot(mod_temp, pred = con, modx = period, plot.points = T) +
-    scale_color_manual(values = c('before' = 'black', 'after' = 'red')) +
-    scale_fill_manual(values = c('before' =  'black', 'after' = 'red')) +
-    scale_linetype_manual(values = c(1,1)) +
-    labs(x = paste0('Control site - log ', clean_names[i]), y = paste0('Test site - log ', clean_names[i])) +
-    annotate('text', x = -Inf, y = Inf, label = rsq_label, vjust = 1.1, hjust = -0.1, parse = T) +
-    annotate('text', x = -Inf, y = Inf, label = p.label, vjust = 2, hjust = -0.04)
-  
-  # save figure 
-  temp_filename <- file.path('figures', paste0(site,'_', site_paired, '_regression_lines.png'))
-  ggsave(temp_filename, p, height = 4, width = 6)
+  mean_pct_change_event <- mean(temp_after$pred_diff_perc)
+  mean_pct_change_sum <- ((sum(temp_after$pred_treatment) - sum(temp_after$pred_baseline))/sum(temp_after$pred_baseline))*100
+
   
 }
 }
