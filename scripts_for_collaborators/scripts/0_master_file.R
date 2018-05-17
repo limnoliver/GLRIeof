8043c7ce5e2e842935b8a227c26f12e148e6e7ae
Oliver <soliver@usgs.gov>
2018-05-17 12:38:42 -0500

    updated variables and variable defaults to reflect new templates.

--- scripts_for_collaborators/scripts/0_master_file.R
+++ scripts_for_collaborators/scripts/0_master_file.R
@@ -4,39 +4,41 @@
 # all processed data files will be exported to the same folder 'cached_data'
 # all figures will be exported to the same folder 'figures'
 
+######## site & study info #####
 # set site-specific information
-site <- '' # site abbreviation that may be used for file naming conventions
-site_no <- '' # USGS site number
-
 study_type <- '' # either "before_after" or "paired"
 
+site <- '' # site abbreviation that may be used for file naming conventions
+           # If this is a paired watershed study, this should be your test or treatment watershed.
+
 # for paired studies
-site_paired <- '' # site abbreviation for paired site - used for file naming conventions and column identification. 
-                  # This should precede your column name for site-specific variables (e.g.)
-site_no_paired <- '' # USGS paired (control) site number, 'NA' if no paired site
+site_paired <- '' # site abbreviation for the paired control site. This will be used in file naming conventions.
 
-control_site <- '' # If a paired study design, the abbreviation of the control site. This should be the same abbreviation as one of the sites listed above.
-test_site <- '' # If a paired study design, the abbreviation of the test, or BMP implementation, site. This should be the same abbreviation as oneof the sites listed above.
+control_site <- 'con' # If a paired study design, the abbreviation of the control site that precedes column names.
+test_site <- 'trt' # If a paired study design, the abbreviation of the test, or BMP implementation, site. This should be the same abbreviation as oneof the sites listed above.
 
-discharge_col <- '' # The name of the discharge columns, if included. Include the column name without 
-                    # the test or control prefix. E.g., if your columns are 'sw1_runoff' and 'sw2_runoff',
-                    # list the discharnge col name as 'runoff'. Use NA if you're not including discharge/runoff values.
-
-# R likes dates in a very specific format (YYYY-MM-DD, coded in R as "%Y-%m-%d"). The scripts need to know 
-# what time zone your input files are in (these should all be the same), and what format your dates are in.
+######## date & times ###################
+# R likes dates in a very specific format (YYYY-MM-DD, coded in R as "%Y-%m-%d"), and if your dates/times
+# are not in the preffered time format, R needs to know that. Set the variables below to reflect your format.
+# R also needs to know  what time zone your input files are in (these should all be the same).
 # To figure out the R format code for your own dates, see this brief tutorial: https://www.r-bloggers.com/date-formats-in-r/ 
 # you can also see all date codes by looking executing ?strptime in your R Console, 
 # and scrolling down to "Details"
+# The defaults below are common ways that Excel exports dates/times.
 
 site_tz <- '' # all data should be converted (or called into R) to the same time zone. 
               # Indicate here which timezone that is - e.g. central "America/Chicago" or central without DST "Etc/GMT+6"
-datetime_format <- '' # format of all datetime columns. For example, 
-date_format <- '' # format of date variables in provided data files. For example, the default in Excel is
-# mm/dd/yyyy, and the coded format of that date in R is "%m/%d/%Y". 
-# dates from Excel are read in the format "" -- but R wants them in the format "YYYY"
-# setting this variable read in NWIS data in the same time zone, but will not verify that all of your data
-# are in a consistent timezone. Please verify that all data are in appropriate time zones.
 
+datetime_format <- "%m/%d/%Y %H:%M:%S" # format of all your datetime columns. For example, if your datetimes are formatted with
+                                       # "mm/dd/yyyy HH:MM:SS" the coded format of the date in R is "%m/%d/%Y %H:%M:%S"
+
+date_format <- "%m/%d/%Y" # format of date variables in provided data files. For example, the default in Excel is
+                          # mm/dd/yyyy, and the coded format of that date in R is "%m/%d/%Y". 
+                          # dates from Excel are read in the format "%m/%d/%Y" -- but R wants them in the format "%Y-%m-%d"
+                          # setting this variable read in NWIS data in the same time zone, but will not verify that all of your data
+                          # are in a consistent timezone. Please verify that all data are in appropriate time zones.
+
+######## BMP timeline ############
 start_date <- as.POSIXct('') # YYYY-MM-DD HH:MM:SS date/time of study start
                              # This can be the first second of the day on the date of the first sampling event
 bmp_date <- as.POSIXct('') # YYYY-MM-DD HH:MM:SS date/time of BMP implementation. All events before
@@ -44,7 +46,7 @@ bmp_date <- as.POSIXct('') # YYYY-MM-DD HH:MM:SS date/time of BMP implementation
                            # date will be considered 'after'
 end_date <- as.POSIXct('') # YYYY-MM-DD HH:MM:SS date/time of study end
 
-######################################
+######## storm event data #############
 ## set wq data parameters
 wq_file <- '' # file name (with .csv extention) where event-level water quality data are stored
 
@@ -58,7 +60,8 @@ wq_file <- '' # file name (with .csv extention) where event-level water quality 
 
 concentrations <- NA # this can either be a vector of verbatim constituent concentrations
 # columns (e.g, c('SS_mg_L', 'TP_mg_L')) or a character string that all concentration
-# columns share in common (e.g., "mg_L"). If you used the template, this should be "mg_L"
+# columns share in common (e.g., "mg_L"). If you used the template and want to evaluate
+# concentrations, this should be "mg_L".
 
 loads <- c('load_pounds') # this can either be a vector of verbatim constituent load
 # columns (e.g, c('SS_load_pounds', 'TP_load_pounds')) or a character string that all load
@@ -98,19 +101,18 @@ event_over_thaw <- # For subevents that span a thaw, you may have to decide whet
                      # set the event to "frozen".
   
   
+  
 ###########################################################################################
 # If you're using a before-after study design, the variables below should be filled out.
 # If you have a paired site, these data aren't necessary, but can be used to run 
 # site diagnostics (yearly precipitation, etc)
 ###########################################################################################
-# rain data
+
+######## rain data ###########
 rain_site <- '' # site number for rain gage if pulling from NWIS, 'NA' if providing data file
 rain_file <- '' # filename of rain file, 'NA' if pulling from NWIS. Should be a csv and include ".csv" at end of file name.
 
-rain_column <- '' # if using own data, the name of the rain value column
-date_column <- '' # if using own data, the name of the date/time column, should be in standard POSIXct (YYY-MM-DD HH:MM:SS)
-
-# field activity data
+######## field activity data #########
 activity_file <- '' # filename of field activity log and should include .csv extension
 nut_additions_keywords <- c('manure', 'fertilizer') # words used to describe any manure and/or fertilizer additions 
                                                     # to the field. These should be keywords found in the "activity_group" column
@@ -127,7 +129,7 @@ antecedent_days <- c(1,2,3,7,14) # the days over which to calculate antecedent d
 dis_date_column <- 'Date' # name of column with dates if providing own file - defaults to NWIS standards
 discharge_column <- 'Flow' # name of column with discharge data if providing own file - defaults to NWIS standards
 
-############# weather data ##############
+######## weather data ##############
 weather_file <- '' # filename (ending in .csv) of daily weather data from a nearby met station to calculate storm
                    # weather characteristics, NA if pulling data directly from NOAA (preferred). 
                    # At a minimum, this file should include 'date' and 
@@ -144,7 +146,7 @@ other_weather_vars <- NA # if you have other daily weather/site characteristic d
                          # the storm spans multiple days. The default behavior can be changed on line 82 in
                          # the file scripts/data_processing/2_calc_weather_variables
 
-########### other ##############
+######## other ##############
 predictors_drop <- '' # one or more predictor variables that should be dropped from the analysis based
                       # on site-specific analysis goals. For example, if you expect that the implemented
                       # BMP will affect field activity (e.g., if the farmer moves to no-till, 'days_since_cultivation'
